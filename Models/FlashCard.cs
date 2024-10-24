20a79c84c7ad6a80e4147a06978aa7746d1518e1
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-25 01:02:44 +0200

    add FlashCard

---
+++ Models/FlashCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Flashcards.Models
+{
+    public class FlashCard
+    {
+        public required string FrontText { get; set; }
+        public required BitmapImage FrontImage { get; set; }
+        public required string BackText { get; set; }
+        public required string BackImage { get; set; }
+    }
+}
