c230579a081529224c4aa2300dc64f101ef589d5
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-25 02:22:50 +0200

    FlashCardSet - Save implementation (without images)

--- Models/FlashCardSet.cs
+++ Models/FlashCardSet.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+using System.IO;
 
 namespace Flashcards.Models
 {
@@ -35,9 +40,24 @@ namespace Flashcards.Models
             FlashCards.Clear();
         }
 
-        public void Save(string filename)
+        public void Save(string fileName)
         {
-            throw new NotImplementedException();
+            JsonObject jsonObject = new JsonObject();
+            JsonArray jsonArray = new JsonArray();
+            jsonObject.Add("FlashCards", jsonArray);
+
+            for (int i = 0; i < FlashCards.Count; i++)
+            {
+                JsonObject newObject = new JsonObject(); ;
+                newObject["FrontText"] = FlashCards[i].FrontText;
+                newObject["FrontImage"] = "";
+                newObject["BackText"] = FlashCards[i].BackText;
+                newObject["BackImage"] = "";
+
+                jsonArray.Add(newObject);
+            }        
+
+            File.WriteAllText(fileName, jsonObject.ToJsonString());
         }
 
         public void Open(string filename)
