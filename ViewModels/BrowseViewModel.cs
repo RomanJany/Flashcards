dd2780e63eea9f5c4b0e5542a4ee1a9306c158f4
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-26 00:10:16 +0200

    add ViewModels

---
+++ ViewModels/BrowseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Notepad.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards.ViewModels
+{
+    public class BrowseViewModel : BaseViewModel
+    {
+
+    }
+}
