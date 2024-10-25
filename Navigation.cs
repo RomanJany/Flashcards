9cf35af100dc07374ac4a89c26a894f8d635537c
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-25 22:55:42 +0200

    add Navigation

---
+++ Navigation.cs
@@ -0,0 +1,32 @@
+﻿using Notepad.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards
+{
+    public class Navigation
+    {
+        private BaseViewModel _currentViewModel;
+        public BaseViewModel CurrentViewModel
+        {
+            get
+            {
+                return _currentViewModel;
+            }
+            set
+            {
+                _currentViewModel = value;
+                OnCurrentViewModelChanged();
+            }
+        }
+
+        public event Action CurrentViewModelChanged;
+        private void OnCurrentViewModelChanged()
+        {
+            CurrentViewModelChanged?.Invoke();
+        }
+    }
+}
