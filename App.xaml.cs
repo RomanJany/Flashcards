38ae1349c3940739a5d0fa4eb1b63b00de3b39c5
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-26 14:19:58 +0200

    App initialization

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using Flashcards.ViewModels;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,25 @@ namespace Flashcards
     /// </summary>
     public partial class App : Application
     {
+        private readonly Navigation _navigation;
+
+        public App()
+        {
+            _navigation = new Navigation();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _navigation.CurrentViewModel = new MenuViewModel();
+
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel(_navigation)
+            };
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 
 }
