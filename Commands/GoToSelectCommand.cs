d9835f9c45cc1b60688b7ea4705cc3416dff9b60
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-26 14:52:02 +0200

    add navigation commands

---
+++ Commands/GoToSelectCommand.cs
@@ -0,0 +1,24 @@
+﻿using Notepad.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards.Commands
+{
+    public class GoToSelectCommand : BaseCommand
+    {
+        private Navigation _navigation;
+
+        public GoToSelectCommand(Navigation navigation)
+        {
+            _navigation = navigation;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
