ca5c9662c4a8751fc210fcacf2ecda3e8fd9caf3
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-26 14:14:39 +0200

    add MainViewModel

---
+++ ViewModels/MainVIewModel.cs
@@ -0,0 +1,20 @@
+﻿using Notepad.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private readonly Navigation _navigation;
+        public BaseViewModel CurrentViewModel => _navigation.CurrentViewModel;
+
+        public MainViewModel(Navigation navigation)
+        {
+            _navigation = navigation;
+        }
+    }
+}
