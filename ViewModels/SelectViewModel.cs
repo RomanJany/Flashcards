8db0d9e7010101f264ab5601eb8f5b6e160cbed7
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-27 15:38:21 +0100

    SelectView - add navigation

--- ViewModels/SelectViewModel.cs
+++ ViewModels/SelectViewModel.cs
@@ -1,9 +1,11 @@
-﻿using Notepad.ViewModels;
+﻿using Flashcards.Commands;
+using Notepad.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Flashcards.ViewModels
 {
@@ -13,6 +15,12 @@ namespace Flashcards.ViewModels
         public SelectViewModel(Navigation navigation)
         {
             _navigation = navigation;
+
+            goToBrowseCommand = new GoToBrowseCommand(_navigation);
+            newFlashCardSetCommand = new NewFlashCardCommand(_navigation);
         }
+
+        public ICommand goToBrowseCommand { get; }
+        public ICommand newFlashCardSetCommand { get; }
     }
 }
