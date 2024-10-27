a9bff7d7295fab39828ebd0500f8fe05f88a223e
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-27 15:36:12 +0100

    add NewFlashCardSetCommand

---
+++ Commands/NewFlashCardSetCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards.Commands
+{
+    public class NewFlashCardSetCommand : BaseCommand
+    {
+        private readonly Navigation _navigation;
+
+        public NewFlashCardSetCommand(Navigation navigation)
+        {
+            _navigation = navigation;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
