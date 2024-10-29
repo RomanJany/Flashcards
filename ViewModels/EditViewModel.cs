818779aeb3cfd9068fc6cfbef442f1cd21581f4c
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-29 16:25:43 +0100

    EditView - add layout

--- ViewModels/EditViewModel.cs
+++ ViewModels/EditViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Flashcards.ViewModels
 {
@@ -15,6 +16,37 @@ namespace Flashcards.ViewModels
         {
             _navigation = navigation;
             _flashCardSet = flashCardSet;
+
+            HasImage = Visibility.Collapsed;
+        }
+
+        private Visibility _hasImage;
+        public Visibility HasImage
+        {
+            get
+            {
+                return _hasImage;
+            }
+            set
+            {
+                _hasImage = value;
+                OnPropertyChanged(nameof(HasImage));
+                OnPropertyChanged(nameof(NHasImage));
+            }
+        }
+        public Visibility NHasImage
+        {
+            get
+            {
+                if (_hasImage == Visibility.Visible)
+                {
+                    return Visibility.Collapsed;
+                }
+                else
+                {
+                    return Visibility.Visible;
+                }
+            }
         }
     }
 }
