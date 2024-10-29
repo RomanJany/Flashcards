178ca910a4aed7d879a03f90e08324d8a06ef1c2
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-29 17:06:16 +0100

    EditViewModel - add current flashcard properties

--- ViewModels/EditViewModel.cs
+++ ViewModels/EditViewModel.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace Flashcards.ViewModels
 {
@@ -19,30 +20,115 @@ namespace Flashcards.ViewModels
             _navigation = navigation;
             _flashCardSet = flashCardSet;
 
-            HasImage = Visibility.Collapsed;
+            CurrentFlashCardIndex = 0;
+            FrontSide = true;
 
             goToSelectCommand = new GoToSelectCommand(_navigation);
         }
 
-        private Visibility _hasImage;
+        private int _currentFlashCardIndex;
+        public int CurrentFlashCardIndex
+        {
+            get
+            {
+                return _currentFlashCardIndex;
+            }
+            set
+            {
+                if (value >= 0 && value < _flashCardSet.FlashCards.Count)
+                {
+                    _currentFlashCardIndex = value;
+
+                    FrontSide = true;
+                }
+            }
+        }
+
+        public FlashCard CurrentFlashCard
+        {
+            get
+            {
+                return _flashCardSet.FlashCards[CurrentFlashCardIndex];
+            }
+        }
+
+        private string _currentText;
+        public string CurrentText
+        {
+            get
+            {
+                return _currentText;
+            }
+            set
+            {
+                _currentText = value;
+                OnPropertyChanged(nameof(CurrentText));
+            }
+        }
+
+        private BitmapImage _currentImage;
+        public BitmapImage CurrentImage
+        {
+            get
+            {
+                return _currentImage;
+            }
+            set
+            {
+                _currentImage = value;
+                OnPropertyChanged(nameof(CurrentImage));
+            }
+        }
+
+        private bool _frontSide;
+        public bool FrontSide
+        {
+            get
+            {
+                return _frontSide;
+            }
+            set
+            {
+                _frontSide = value;
+                _flashCardSideUpdate();
+                OnPropertyChanged(nameof(HasImage));
+                OnPropertyChanged(nameof(NHasImage));
+            }
+        }
+
+        private void _flashCardSideUpdate()
+        {
+            if (FrontSide)
+            {
+                CurrentText = CurrentFlashCard.FrontText;
+                CurrentImage = CurrentFlashCard.FrontImage;
+            }
+            else
+            {
+                CurrentText = CurrentFlashCard.BackText;
+                CurrentImage = CurrentFlashCard.BackImage;
+            }
+        }
+
         public Visibility HasImage
         {
             get
             {
-                return _hasImage;
-            }
-            set
-            {
-                _hasImage = value;
-                OnPropertyChanged(nameof(HasImage));
-                OnPropertyChanged(nameof(NHasImage));
+                if (CurrentImage == null)
+                {
+                    return Visibility.Collapsed;
+                }
+                else
+                {
+                    return Visibility.Visible;
+                }
             }
         }
         public Visibility NHasImage
         {
             get
             {
-                if (_hasImage == Visibility.Visible)
+                if (HasImage == Visibility.Visible)
                 {
                     return Visibility.Collapsed;
                 }
